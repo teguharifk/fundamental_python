60e784e0b06fa9f0b1d8f034bb597bf51cde01cc
Teguh Arif <>
2021-02-24 09:48:00 +0700

    training 2

--- fundamental2.py
+++ fundamental2.py
@@ -1,3 +1,5 @@
+import requests
+
 kamus = {}
 kamus['anak'] = 'son'
 kamus['istri'] = 'wife'
@@ -5,3 +7,23 @@ kamus['ayah'] = 'father'
 
 print(kamus)
 print(kamus['istri'])
+
+def hitung_luas_segitiga(alas, tinggi):
+    luas_segitiga = alas * tinggi / 2
+    return luas_segitiga
+
+print(hitung_luas_segitiga(13,2))
+print(hitung_luas_segitiga(73,4))
+
+url = 'https://detik.com'
+try:
+    response = requests.get(url)
+    if response.status_code == 200 :
+        print('sukses')
+        print(response.text)
+    else:
+        print(f'ada kesalahan {response.status_code}')
+except Exception as e:
+    print('gagal')
+
+
---
+++ use_geometri.py
@@ -0,0 +1,6 @@
+from geometri.segitiga import hitung_luas_segitiga
+# import geometri.segitiga as gs
+
+result = hitung_luas_segitiga(18,3)
+
+print(f'hasil luas segitiga {result}')
